9866e388b097c721e249744015641c4dbd695db0
abdelmouttaleb <abdelmouttalebdakri@gmail.com>
2020-11-22 20:29:26 +0100

    second commit

--- Laplacian.cpp
+++ Laplacian.cpp
@@ -26,6 +26,7 @@ VectorXd Laplacian::InitialCondition()
   Nx=_df -> Get_Nx();
   Ny=_df -> Get_Ny();
   VectorXd cond_init;
+  cond_init.resize(Nx*Ny);
 
   for (int i=0; i<Nx; i++ )
   {
@@ -65,7 +66,7 @@ MatrixXd Laplacian::BuildLaplacianMatrix()
   b=-1/(hy*hy);
   SparseMatrix<double> T(Nx,Ny);
   vector<Triplet<double>> triplets1;
-  for (int i=0; i<B.rows(); ++i)
+  for (int i=0; i<T.rows(); ++i)
   {
     triplets1.push_back({i,i,b});
     if (i > 0)
@@ -86,8 +87,29 @@ MatrixXd Laplacian::BuildLaplacianMatrix()
 
 void Laplacian::BuildSourceTerm(double t)
 {
+  int Nx,Ny;
+  double xmin,xmax,ymin,ymax,hx,hy;
+  xmin= _df -> Get_xmin();
+  // xmax= _df -> Get_xmax();
+  ymin= _df -> Get_ymin();
+  // ymax= _df -> Get_ymax();
+  hx= _df ->Get_hx();
+  hy= _df ->Get_hy();
 
+  Nx=_df -> Get_Nx();
+  Ny=_df -> Get_Ny();
+  VectorXd source_term(Nx*Ny);
+    for (int i=0; i<Nx; i++ )
+  {
+    for(int j=0; j<Ny; j++)
+    {
+      source_term(i+j*Nx)=_fct -> SourceFunction(xmin+i*hx,ymin+j*hy,t);
+    }
+  }
+  return source_term;
 };
+
+
 MatrixXd Laplacian::Get_Matrix(){};
 
 
@@ -106,7 +128,7 @@ VectorXd Laplacian::Getsource_term()
   Ny=_df -> Get_Ny();
   VectorXd source_term(Nx*Ny);
 
-  //jsute por faire fonctionner
+  //juste por faire fonctionner
   double t=0.;
 
   for (int i=0; i<Nx; i++ )
